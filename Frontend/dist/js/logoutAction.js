a5eefa64f2cbb8d6104377a3cbfafc74a4eb56c9
nikoicarl <cnikoi@logig.tech>
2024-05-26 10:42:31 -0700

    timeline css added

--- Frontend/dist/js/logoutAction.js
+++ Frontend/dist/js/logoutAction.js
@@ -20,7 +20,6 @@
     //Get logout response
     socket.on(melody.melody1+'_logoutAction', function(data) {
         if (data.type == "error") {
-            console.log(data.message);
             $('.ps_logout_btn').html(`
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
